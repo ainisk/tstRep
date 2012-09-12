32feb53a26cb43a39cd5cbc133b7bd089bf1087c
ainisk <ainars.kumpins@gmail.com>
2012-09-12 17:39:34 +0300

    server edit 5

--- source/JavaApplication5/src/javaapplication5/JavaApplication5.java
+++ source/JavaApplication5/src/javaapplication5/JavaApplication5.java
@@ -14,6 +14,7 @@ public class JavaApplication5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // server edit 5
         // TODO code application logic here
     }
 }
